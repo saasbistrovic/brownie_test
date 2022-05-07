1b66bf3a9c7edff8f9b83f1ab489256e1145b886
saasbistrovic <86487063+saasbistrovic@users.noreply.github.com>
2022-05-07 15:21:08 +0200

    Add files via upload

---
+++ scripts/deploy.py
@@ -0,0 +1,4 @@
+from brownie import SimpleStorage, accounts
+def main():
+    account = accounts.load('deployment_account')
+    SimpleStorage.deploy({'from': account})
---
+++ tests/test_storage.py
@@ -0,0 +1,6 @@
+# File: test_storage.py #
+def test_storage(SimpleStorage, accounts):
+    simple_storage = SimpleStorage.deploy({'from': accounts[0]})
+    assert simple_storage.get() == 0
+    simple_storage.set(5)
+    assert simple_storage.get() == 5
